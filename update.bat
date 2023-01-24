c6207c71c30da1e022d6f290ba5e45bd691d2475
flyingmusicnote <aaronkanterkjv@gmail.com>
2023-01-23 21:07:11 -0500

    commit

--- update.bat
+++ update.bat
@@ -5,6 +5,6 @@ git init
 git add -A
 git commit -am "commit"
 git branch -M main
-git remote add origin https://github.com/flyingmusicnote/flyingmusicnote.github.io.git
+git remote add origin https://github.com/FlyingMusicNote/FlyingMusicNote.github.io.git
 git push -u origin main
 pause

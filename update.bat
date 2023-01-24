7399a2787a6b00175773035955b448896338e1a1
Aaron K <aaronkanterkjv@gmail.com>
2023-01-23 20:01:25 -0500

    commit

--- update.bat
+++ update.bat
@@ -2,6 +2,5 @@ git init
 git add -A
 git commit -am "commit"
 git branch -M main
-git remote add origin https://github.com/nicholascz0/nicholascz0.github.io/quandaledingle.git
 git push -u origin main
 pause

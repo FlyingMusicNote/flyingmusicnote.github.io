ce119ff449357e47d66cddc97ea3e14733f3e83d
flyingmusicnote <aaronkanterkjv@gmail.com>
2023-01-24 13:17:16 -0500

    commit

--- fnf-song-converter/update.bat
+++
@@ -1,8 +0,0 @@
-:: A bat file that goes into the files of your github website, this file updates the contents of your github repo when you run it
-:: #fmn
-
-git add .
-git commit -m "commit" 
-git remote add origin https://github.com/FlyingMusicNote/fnf-song-converter.git
-git push -u origin main
-pause

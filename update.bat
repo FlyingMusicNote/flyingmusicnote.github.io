6b9fb87b650f4929fc7d4a1997d4786a133ecac5
flyingmusicnote <aaronkanterkjv@gmail.com>
2023-01-23 23:30:11 -0500

    commit

--- update.bat
+++ update.bat
@@ -1,9 +1,9 @@
 :: A bat file that goes into the files of your github website, this file updates the contents of your github repo when you run it
 :: #fmn
 
-git init
-git add -A
-git commit -am "commit"
-git branch -M main
-git push -u origin main
+git add .
+git commit -m "commit" 
+git remote add origin https://github.com/flyingmusicnote/flyingmusicnote.github.io.git
+git push -u origin master
+git status
 pause
